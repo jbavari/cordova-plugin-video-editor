e8989ab8c021cc053a29abd8eba949d758254381
Josh Bavari <josh@raisemore.com>
2014-01-16 23:51:15 -0600

    Added constants in there for quality, format, and network use optimization

---
+++ www/VideoEditorConstants.js
@@ -0,0 +1,25 @@
+// VideoEditorConstants.js
+
+var VideoEditorOptions = {
+	Quality: {
+		HIGH_QUALITY: 0
+		MEDIUM_QUALITY: 1,
+		LOW_QUALITY: 2,
+	},
+	OptimizeForNetworkUse = {
+		NO: 0,
+		YES: 1
+	},
+	OutputFileType: {
+		M4V: 0,
+		MPEG4: 1,
+		M4A: 2,
+		QUICK_TIME: 3
+	}
+};
+
+if (typeof exports !== 'undefined') {
+  if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = VideoEditorOptions;
+  }
+}
