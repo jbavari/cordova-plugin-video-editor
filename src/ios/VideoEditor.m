55907f46da09cbe244669adae993ee1d19409982
Oliver Schulze <oliver.schulze@agorum.com>
2019-12-02 21:49:59 +0100

    fixed problem when transcoding hevc videos (error -11500)

--- src/ios/VideoEditor.m
+++ src/ios/VideoEditor.m
@@ -150,7 +150,7 @@
         AVVideoCompressionPropertiesKey: @
         {
             AVVideoAverageBitRateKey: [NSNumber numberWithInt: videoBitrate],
-            AVVideoProfileLevelKey: AVVideoProfileLevelH264High41
+            AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel
         }
     };
     encoder.audioSettings = @
